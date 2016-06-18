fe6e7a4993a7167f40978a06a1eff589dabdf1e2
= <=>
2016-06-18 06:49:36 -0400

    added chess sprites to gui_images and added refreshBoard mutator to gui class

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
-import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -12,9 +11,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
 
 import data_types.Board;
+import data_types.Coordinate;
+import data_types.Piece;
+import data_types.PieceColor;
 
 /**
  * Represents a Chess GUI
@@ -57,21 +58,7 @@ public class GUI extends JFrame{
         this.chessBoardSquares = new JButton[BOARD_SIZE][BOARD_SIZE];
         this.chessBoard = new JPanel(new GridLayout(GRID_SIZE, GRID_SIZE));
         
-        // create the chess buttons
-        Insets buttonMargin = new Insets(0, 0, 0, 0); // default button margin is not visible
-        for (int row = 0; row < chessBoardSquares.length; row++) {
-            for (int col = 0; col < chessBoardSquares.length; col++) {
-                JButton button = new JButton();
-                button.setMargin(buttonMargin);
-                
-                ImageIcon icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_queen.png"));
-                button.setIcon(icon);
-                
-                button.setBackground(getSquareColor(row, col));
-                
-                chessBoardSquares[row][col] = button;
-            }
-        }
+        refreshBoard();
         
         // fill in the chessBoard
         chessBoard.add(new JLabel(""));
@@ -108,15 +95,78 @@ public class GUI extends JFrame{
     }
     
     /**
+     * Refreshes the gui to be updated with current board
+     */
+    public void refreshBoard() {
+     // create the chess buttons
+        Insets buttonMargin = new Insets(0, 0, 0, 0); // default button margin is not visible
+        PieceColor white = PieceColor.WHITE;
+        PieceColor black = PieceColor.BLACK;
+        for (int row = 0; row < chessBoardSquares.length; row++) {
+            for (int col = 0; col < chessBoardSquares.length; col++) {
+                JButton button = new JButton();
+                button.setMargin(buttonMargin);
+                
+                Piece piece = board.getSquare(new Coordinate(7-row, 7-col)).getPiece();
+                
+                ImageIcon icon = new ImageIcon();
+                if (piece.exists()) {
+                    if (piece.color().equals(white)) {
+                        if (piece.isPawn()) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "white_pawn.png"));
+                        } else if (piece.isKing()) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "white_king.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.queen(white, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "white_queen.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.rook(white, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "white_rook.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.knight(white, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "white_knight.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.bishop(white, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "white_bishop.png"));
+                        } else {
+                            throw new RuntimeException("Can not recognize piece");
+                        }
+                    } else if (piece.color().equals(black)) {
+                        if (piece.isPawn()) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_pawn.png"));
+                        } else if (piece.isKing()) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_king.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.queen(black, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_queen.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.rook(black, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_rook.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.knight(black, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_knight.png"));
+                        } else if (piece.getMovedVersion().equals(Piece.bishop(black, true))) {
+                            icon = new ImageIcon(getClass().getResource(GUI_IMAGES + "black_bishop.png"));
+                        } else {
+                            throw new RuntimeException("Can not recognize piece");
+                        }
+                    } else {
+                        throw new RuntimeException("Piece's color is not one of black or white");
+                    }
+                }
+                
+                button.setIcon(icon);
+                
+                button.setBackground(getSquareColor(row, col));
+                
+                chessBoardSquares[row][col] = button;
+            }
+        }
+    }
+    
+    /**
      * Retrieve the square color for this coordinate
      * @param row row of square
      * @param col column of square
      * @return Color.WHITE if the (row, col) coordinate is white on a normal chess board,
-     *     and Color.BLACK if the (row, col) coordinate is black on a normal chess board
+     *     and Color.BLUE if the (row, col) coordinate is black on a normal chess board
      *     
      *      - this coordinate system is shifted to right, to take into account for the 
      *         coordinate system axis. Thus, the a1 square is the (1, 0) coordinate
-     *      - therefore, (1, 0) should return Color.BLACK, (1, 1) should return Color.WHITE, etc.
+     *      - therefore, (1, 0) should return Color.BLUE, (1, 1) should return Color.WHITE, etc.
      */
     private Color getSquareColor(final int row, final int col) {
         boolean sameParity = row%2 == col%2;
@@ -124,7 +174,7 @@ public class GUI extends JFrame{
         if (sameParity) {
             return Color.WHITE;
         } else {
-            return Color.BLACK;
+            return Color.BLUE;
         }
     }
     
